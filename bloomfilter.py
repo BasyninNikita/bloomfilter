f89d5a2cd2097dd3d0724b69ad33d737920e9514
BasyninNikita <32190908+BasyninNikita@users.noreply.github.com>
2019-10-21 21:38:31 +0300

    Update bloomfilter.py

--- bloomfilter.py
+++ bloomfilter.py
@@ -1,7 +1,7 @@
 import math
 import sys
 
-M = 216091  # M-31th Mersen's number
+M = (1 << 31) - 1  # M-31th Mersen's number
 
 
 def get_command():
@@ -18,14 +18,14 @@ def get_command():
                 print('error')
         elif 'add' in line:
             if len(line.replace('add', '').strip().split(
-                    ' ')) == 1 and ss :  # line.replace('push ', '')[:-1] != 1 and ss != 0:
+                    ' ')) == 1 and ss:  # line.replace('push ', '')[:-1] != 1 and ss != 0:
                 bf.add(int(line.split()[1]))
                 print('', end='')
             else:
                 print('error')
         elif 'search' in line:
             if len(line.replace('search', '').strip().split(
-                    ' ')) == 1 and ss :  # line.replace('push ', '')[:-1] != 1 and ss != 0:
+                    ' ')) == 1 and ss:  # line.replace('push ', '')[:-1] != 1 and ss != 0:
                 print(bf.search(int(line.split()[1])), end='')
             else:
                 print('error')
@@ -63,14 +63,42 @@ def prime(k):
     return (i + 1)
 
 
+def hashes(key, num_hashes, m):
+    hashs = list()  # (((i + 1)*x + pi+1) mod M) mod m ne pi,a p i+1-oe
+    for i in range(num_hashes):
+        hashs.append((((i + 1) * key + prime(i + 1)) % M) % m)  # M-31th Mersen's number
+    return hashs
+
+
+class Bits:
+
+    def __init__(self, m, num_hashes):
+        self.m = m
+        self.num_hashes = num_hashes
+        self.bits = 0
+
+    def add(self, key):
+        for i in hashes(key, self.num_hashes, self.m):
+            self.bits = self.bits | (1 << i)
+
+    def print(self):
+        return (bin(self.bits)[:1:-1]) + '0' * (self.m - len(bin(self.bits)[2:])) + '\n'
+
+    def search(self, key):
+        for i in hashes(key, self.num_hashes, self.m):
+            if (self.bits & (1 << i)) == 0:
+                return '0\n'
+        return '1\n'
+
+
 class BloomFilter:
 
     def __init__(self, n, P):
-        self.n = n
-        self.P = P
+        # self.n = n
+        # self.P = P
         self.num_hashes = round(-(math.log2(P)))
         self.m = round(- (n * math.log2(P)) / math.log(2))
-        self.bits = 0
+        self.bits = Bits(self.m, self.num_hashes)
         # print(str(self.m) + ' ' + str(self.num_hashes))
 
     def size(self):
@@ -79,30 +107,26 @@ class BloomFilter:
     def num_hashs(self):
         return self.num_hashes
 
-    def hashes(self, key):
-        hashs = list()  # (((i + 1)*x + pi+1) mod M) mod m ne pi,a p i+1-oe
-        for i in range(self.num_hashes):
-            hashs.append((((i + 1) * key + prime(i + 1)) % M) % self.m)  # M-31th Mersen's number
-        return hashs
-
     def add(self, key):
-        for i in self.hashes(key):
-            self.bits = self.bits | 2 ** i
+        # for i in hashes(key, self.num_hashs):
+        #     self.bits = self.bits | (1 << i)
+        return self.bits.add(key)
 
     def search(self, key):
-        for i in self.hashes(key):
-            if (self.bits & 2 ** i) == 0:
-                return '0\n'
-        return '1\n'
+    #     for i in hashes(key, self.num_hashs):
+    #         if (self.bits & (1 << i)) == 0:
+    #             return '0\n'
+    #     return '1\n'
+        return self.bits.search(key)
 
     def print(self):
-        s = ''
-        # for i in range(self.m - len(bin(self.bits)[2:])):
-        #     s += ('0')
-        s += '0' * (self.m - len(bin(self.bits)[2:]))
-        s += (bin(self.bits)[:1:-1]) + '\n'
-        return s
+        # s = ''
+        # # for i in range(self.m - len(bin(self.bits)[2:])):
+        # #     s += ('0')
+        # s += '0' * (self.m - len(bin(self.bits)[2:]))
+        # s += (bin(self.bits)[:1:-1]) + '\n'
+        # return (bin(self.bits)[:1:-1]) + '0' * (self.m - len(bin(self.bits)[2:])) + '\n'
+        return self.bits.print()
 
 
 get_command()
-
